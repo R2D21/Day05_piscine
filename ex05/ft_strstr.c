0cc58a92dca31e3fe39a52cdf52753e3bc1dde69
Remi BOIVIN <rboivin@e2r9p12.42.fr>
2018-08-14 12:26:34 +0200

    third commit

--- ex05/ft_strstr.c
+++ ex05/ft_strstr.c
@@ -1,15 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rboivin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/08/11 08:30:28 by rboivin           #+#    #+#             */
-/*   Updated: 2018/08/14 11:57:13 by rboivin          ###   ########.fr       */
+/*   Created: 2018/08/14 11:24:54 by rboivin           #+#    #+#             */
+/*   Updated: 2018/08/14 12:25:33 by rboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+int			ft_strlen(char *str)
+{
+	int		i;
+
+    i = 0;
+    while(str[i])
+        i++;
+    return i;
+}
+ 
 int		ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int		i;
@@ -24,4 +36,24 @@ int		ft_strncmp(char *s1, char *s2, unsigned int n)
 		i++;
 	}
 	return (s1[i] - s2[i]);
+}
+
+char		*ft_strstr(char *str, char *to_find)
+{
+    while (*str)
+    {
+        if (ft_strncmp(str, to_find, ft_strlen(to_find)) == 0)
+            return (str);
+		str++;
+    }
+    return NULL;
+}
+
+int		main()
+{
+	char	str[100] = "je suis un un chat et je mange la souris";
+	char	str1[100] = "la souris";
+	
+	printf("%s\n", ft_strstr(str, str1));
+	return (0);
 }
